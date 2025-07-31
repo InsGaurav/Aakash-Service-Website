24c29c874795866abc751dd99738b18390383e2d
InsGaurav <gaurav@cvs.du.sc.in>
2025-07-31 10:23:21 +0530

    dfdg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: '',
+  base: import.meta.env.MODE === 'production' ? '/Aakash-Service-Website/' : '/',
   plugins: [react()],
 });
